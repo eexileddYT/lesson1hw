192e443c29a92712d11a93d5859884c6136c260b
eexileddYT <87630983+eexileddYT@users.noreply.github.com>
2021-07-20 11:21:20 +0800

    Add files via upload

---
+++ python hw/lesson1_hw.py
@@ -0,0 +1,14 @@
+
+
+#Preston Tsai 蔡侑廷
+
+print ('Please enter a radius of your choice')
+radius = input()
+
+pi = 3.14
+C = float (2) * pi * float (radius)
+print('Circumference =', C)
+
+
+A = pi * float (radius) * float (radius)
+print('Area =', A)
